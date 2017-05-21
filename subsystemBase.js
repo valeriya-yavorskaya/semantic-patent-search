c24a6f6e9bf374111f8d41c53b28442d1c4c7b6e
YAVORSKAYA-PC\Валерия <valeriya.yavorskaya@gmail.com>
2017-05-21 19:24:31 +0400

    some files were deleted

--- subsystemBase.js
+++
@@ -1,9 +0,0 @@
-// function launchSolarix() {
-//     console.log('here');
-//     $.get('http://localhost:8888/launch-solarix', {
-//     }).then(function (res) {
-//         console.log('Solarix was launched');
-//     }, function (reason) {
-//         console.log(reason);
-//     });
-// }
