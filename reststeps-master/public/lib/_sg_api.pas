0ebc8cf3a64d761b6c32067484032b2710caea6b
YAVORSKAYA-PC\Валерия <valeriya.yavorskaya@gmail.com>
2017-05-21 20:31:07 +0400

    large files were rebased

---
+++ reststeps-master/public/lib/_sg_api.pas
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a75bd8d0e306f0c83a4d389d511b648b15684c0d0402c8c0b8244eece58766d9
+size 28424
---
+++ reststeps-master/public/lib/pro/_sg_api.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:13e6210b762de59d2255202dfb5383c9bcaaa14543fab91e2da4502900fa38ad
+size 28905
